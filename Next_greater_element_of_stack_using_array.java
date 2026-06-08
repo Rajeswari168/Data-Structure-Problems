be24befed184582909d30bb5fe641c822e9b6bea
Rajeswari168 <mrajeswariraji66@gmail.com>
2026-06-08 18:38:25 +0530

    Add Next Greater Element program using stack
    Implemented a program to find the next greater element for each element in an array using a stack.

---
+++ Next_greater_element_of_stack_using_array.java
@@ -0,0 +1,34 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        int[] result = new int[n];
+        for(int i = 0; i < n; i++){
+            arr[i] = sc.nextInt();
+        }
+        for(int i = 0; i < n-1; i++){
+            for(int j = 0; j < n-i-1; j++){
+                if(arr[j] > arr[j+1]){
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                }
+            }
+        }
+        for(int i = 0; i < n; i++){
+            result[i] = -1;
+        }
+        Stack<Integer> st = new Stack<>();
+        for(int i = 0; i < n; i++){
+            while(!st.isEmpty() && arr[i] > arr[st.peek()]){
+                result[st.pop()] = arr[i];
+            }
+            st.push(i);
+        }
+        for(int i = 0; i < n; i++){
+            System.out.print(result[i] + " ");
+        }
+    }
+}
